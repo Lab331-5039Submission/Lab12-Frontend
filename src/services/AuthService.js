d9f5985850a02e798c5820eeb15752338ab9c330
Oralita <oat431@gmail.com>
2021-10-11 08:39:33 +0700

    [6.8] : full authentication

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -23,7 +23,21 @@ import GStore from '@/store'
               },
         getUser(){
             return JSON.parse(localStorage.getItem('user'))
+        },
+        hasRoles(roles) {
+                if (GStore.currentUser && roles) {
+                  let containRoles = GStore.currentUser.authorities.filter((authority) => roles.includes(authority))     
+                  if (containRoles.length > 0){
+                    return true
+                  }else{
+                    return false
+                  }
+                 
+                } else {
+                  return false
+                }
         }
+            
 
     }
     
